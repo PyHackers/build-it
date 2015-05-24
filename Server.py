2907aa4a3def7c6575d21cde6963357f30a7e35e
Tarun Bansal <tarunbansal.me@gmail.com>
2015-05-24 07:43:12 +0530

    Added Python app code new

--- Server.py
+++ Server.py
@@ -15,6 +15,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
+	return "Hello World"
+
+@app.route("/upload")
+def upload():
+	#fileUnzipper(file)
 	cli = Client('unix://var/run/docker.sock')
 	containers = cli.containers()
 	images = cli.images()
@@ -23,26 +28,39 @@ def hello():
 	sContID = SERVER_CONFIG.get('SECONDARY_CONTAINER','id')
 	for container in containers:
 		if container["Id"] == pContID:
-			createdCont = cli.create_container(image=container["Image"], command=str(pCmnd))
+			createdCont = cli.create_container(image=container["Image"], command=str(pCmnd), tty=True)
+			response = cli.start(container=createdCont.get('Id'))
+			sContID = str(createdCont.get('Id'))
+			print " Started copy of primary container " + str(createdCont.get('Id')) + " Now going to redirect traffic! "
 			continue
-	return "Hello World" + str("\n")  + str(containers) + str(pContID) + str(sContID) + str(createdCont)
+	#redirecSecTraffic()
+	#scriptExecutor(pContID)
+	#redirecPriTraffic()
+	print " Stopping and removing secondary container created "
+	cli.stop(sContID)
+	remRsp = cli.remove_container(sContID, force=True)
+	print str(remRsp)
+	return "Hello World" + str("\n")  + str(containers) + str(pContID) + str(sContID) + str(response)
 
 def scriptExecutor(pContID):
 	destPath = SERVER_CONFIG.get('BUILD_DETAILS','path')
+	print " Going to execute script and update build in primary "
 	out = os.system("sh extract.sh " +  str(pContID) +  " " + str(destPath))
 	return out
 
 def fileUnzipper(file):
+	print " Unzipping files recieved in current directory in host "
 	zf = zipfile.ZipFile(file)
 	zf.extractall("../data/")
 
-def redirectTraffic():
+def redirectSecTraffic():
 	pass
 
-def closeContainer():
-	pass
+def removeContainer():
+	print " Stopping and removing secondary container created "
 
-def redirectTraffic():
+
+def redirectPriTraffic():
 	pass
 
 if __name__ == "__main__":
